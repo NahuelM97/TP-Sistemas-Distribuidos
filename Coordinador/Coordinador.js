efbe6661b2ea4d40506969c269b0b72dde702453
JPCard <juampicar97@gmail.com>
2020-11-12 22:02:29 -0300

    Agrega modulo de globals para funciones y constantes generales, y refactor

--- Coordinador/Coordinador.js
+++ Coordinador/Coordinador.js
@@ -1,4 +1,5 @@
-var zmq = require('zeromq');
+const zmq = require('zeromq');
+const globals = require('../Global/Globals');
 
 // socket to talk to clients
 var repSocket = zmq.socket('rep');
@@ -7,24 +8,6 @@ var reqSocket = zmq.socket('req');
 
 const ipCoordinador = `127.0.0.1`;
 const puertoCoordinador = 1234;
-
-//CLIENTE -> COORDINADOR
-const COD_PUB = 1; // Cliente publica un nuevo mensaje
-
-const COD_ALTA_SUB = 2; // Cliente se suscribe a un tópico
-
-
-//COORDINADOR -> BROKER
-const COD_ADD_TOPICO_BROKER = 3;// Coordinador informa al broker un nuevo topico 
-
-
-//SERVIDOR HTTP -> BROKER
-const COD_GET_TOPICOS = 4; // Servidor solicita a todos los sus topicos 
-
-const COD_BORRAR_MENSAJES = 6; // Servidor solicita a un broker que borre sus mensajes
-
-//SERVIDOR HTTP -> TODOS LOS BROKERS
-const COD_GET_MENSAJES_COLA = 5;// Servidor solicita a todos los brokers todos sus mensajes
 
 
 //key: el topico
@@ -44,7 +27,8 @@ let brokerIpPuerto = [
 let contadorTopicosPorBroker = [0, 0, 0];
 
 // Guarda las solicitudes pendientes
-var pendingRequests = {};
+var pendingRequests = {}; //TODO hacer esto porque no esta hecho
+// aca van las request en el orden en que llegaron tipo cola
 
 // ---------------------------------------- Main ------------------------------------------
 // Se inicia la escucha 
@@ -53,69 +37,39 @@ var pendingRequests = {};
 }
 
 function obtieneBrokersIniciales(){
-    // Obtengo el de heartbeat
+    let reqSocketInit = zmq.socket('req');
 
-    let mensaje = {
-        idPeticion: generateUUID(),
-        accion: COD_ADD_TOPICO_BROKER,
-        topico: topico
-    }
-    let brokerHeartbeat = brokerIpPuerto[0];
-    let brokerHeartbeatIpPuerto = `tcp://${brokerHeartbeat.ip}:${brokerHeartbeat.puertoRep}`;
-    reqSocket.connect(brokerHeartbeatIpPuerto);
+    reqSocketInit.on('message', cbSocketInit);
 
-    // TODO: Obtener el de message/all
+    enviarAsignacionTopicoBroker(reqSocketInit, 'heartbeat', false);
+    enviarAsignacionTopicoBroker(reqSocketInit, 'message/all', false);
 
-    // TODO: Una vez que las dos conexiones estén confirmadas,
-    repSocket.bind(`tcp://${ipCoordinador}:${puertoCoordinador}`);
+    reqSocket.on('message', cbAsignaTopicoBroker);
+    repSocket.on('message', cbRespondeRequest);
 }
 
-function getBrokerMenosTopicos() {
-    let minCantTopicos = Math.min(...contadorTopicosPorBroker);
-    return contadorTopicosPorBroker.indexOf(minCantTopicos);
-}
-
-//Envia respuesta al cliente del broker, del topico solicitado
-function enviarBrokerSegunTopicoExistente(topico,isPub){
-    
-    let idBroker = topicoIdBroker[topico];
-    let ipPuertoBroker = brokerIpPuerto[idBroker];
-
-
-    let respuesta = {
-        ip: ipPuertoBroker.ip,
-        puerto: isPub ? ipPuertoBroker.puertoPub : ipPuertoBroker.puertoSub,
-        topico: topico
-    }
-
-    repSocket.send(JSON.stringify(respuesta));
-}
-
-function enviarBrokerSegunTopicoNoExistente(topico,isPub){
-    let mensaje = {
-        idPeticion: generateUUID(),
-        accion: COD_ADD_TOPICO_BROKER,
-        topico: topico
-    }
-    // TODO: Hacer esto porque no está hecho
-    let broker = getBrokerMenosTopicos();
-    reqSocket.connect(); // ?
-    reqSocket.send(JSON.stringify(mensaje))
-    // fin del todo
-}
-       
-reqSocket.on('message',function(responseJSON){ //Cuando el broker responde la solicitud de creacion del topico
+function cbAsignaTopicoBroker(responseJSON) { //Cuando el broker responde la solicitud de creacion del topico
     let response = JSON.parse(responseJSON);
     // Respuesta exitosa
-    if(response && response.exito){
-        if(pendingRequests.hasOwnProperty(response.requestId)){
+    if (response && response.exito) {
+        if (pendingRequests.hasOwnProperty(response.requestId)) {
             topicoIdBroker[topico] = idBrokerMenosTopicos;
             contadorTopicosPorBroker[idBrokerMenosTopicos]++;// TODO PREGUNTAR peligro concurrencia
-            
+
             //nadie lo tiene, se lo asignamos a alguien
-            
+
             //avisarle al topico que se le asigno (tiene que confirmar?)  
-            enviarBrokerSegunTopicoExistente(topico,isPub);
+            
+
+            switch (request.accion) {
+                case globals.COD_PUB:
+                    enviarBrokerSegunTopicoExistente(topico, isPub);
+                    break;
+                case globals.COD_ALTA_SUB:
+                    enviarDatosAlta(topico, false); //TODO preguntar en que contexto de ejecucion se ejecutan los callbacks
+                    break;
+                default:
+            }
         }
         else {
             // TODO: Llegó una rta de una request que no hice!
@@ -124,18 +78,18 @@ reqSocket.on('message',function(responseJSON){ //Cuando el broker responde la so
     else {
         // TODO: Mandar respuesta al cliente de que no hubo exito.
     }
-})
+}
 
-repSocket.on('message', function (requestJSON) { //un cliente quiere saber donde estan 1 o 3 topicos
+function cbRespondeRequest(requestJSON) { //un cliente quiere saber donde estan 1 o 3 topicos
     // Paso a objeto la request
     let request = JSON.parse(requestJSON);
 
     // Se deriva a la funcion que procesa la solicitud según corresponda
     switch (request.accion) {
-        case COD_PUB: 
+        case globals.COD_PUB:
             procesarSolicitudPublicacion(request)
             break;
-        case COD_ALTA_SUB: 
+        case globals.COD_ALTA_SUB:
             procesarAltaCliente(request);
             break;
         default:
@@ -143,7 +97,68 @@ repSocket.on('message', function (requestJSON) { //un cliente quiere saber donde
         //algo salio mal esto no tendria que estar aca.
     }
     console.log("Received request: [", request, "]");
-});
+}
+
+
+
+
+// asignamos los topicos heartbeat y message/all antes de empezar a atender request
+function cbSocketInit() {
+    //Cuando el broker responde la solicitud de creacion del topico
+    let response = JSON.parse(responseJSON);
+
+    if (response && response.exito) {
+        topicoIdBroker[topico] = idBrokerMenosTopicos;
+        contadorTopicosPorBroker[idBrokerMenosTopicos]++;
+        if (getCantKeys(topicoIdBroker) == 2) { // ya se asignaron heartbeat y message/all a un broker.
+            repSocket.bind(`tcp://${ipCoordinador}:${puertoCoordinador}`); // aca empieza a escuchar requests
+        }
+    }
+    else {
+        // TODO: Mandar respuesta al cliente de que no hubo exito.
+    }
+}
+
+
+
+
+
+function getBrokerMenosTopicos() {
+    let minCantTopicos = Math.min(...contadorTopicosPorBroker);
+    return contadorTopicosPorBroker.indexOf(minCantTopicos);
+}
+
+// Envia respuesta al cliente del broker, del topico solicitado porque ya estaba asignado a un broker
+function enviarBrokerSegunTopicoExistente(topico,isPub){
+    
+    let idBroker = topicoIdBroker[topico];
+    let ipPuertoBroker = brokerIpPuerto[idBroker];
+
+
+    let respuesta = {
+        ip: ipPuertoBroker.ip,
+        puerto: isPub ? ipPuertoBroker.puertoSub : ipPuertoBroker.puertoPub, // me tengo que comunicar con la parte opuesta del protocolo
+        topico: topico
+    }
+
+    repSocket.send(JSON.stringify(respuesta));
+}
+
+// le avisamos a un broker que tiene un topico nuevo asignado
+function enviarAsignacionTopicoBroker(socket,topico,isPub){
+    let mensaje = {
+        idPeticion: globals.generateUUID(),
+        accion: globals.COD_ADD_TOPICO_BROKER,
+        topico: topico
+    }
+
+    let broker = getBrokerMenosTopicos();
+    socket.connect(`tcp://${broker.ip}:${broker.puertoRep}`); 
+    socket.send(JSON.stringify(mensaje));
+    
+}
+       
+
 
 // TODO: Terminar
 // Funciones que procesan las solictudes
@@ -154,7 +169,7 @@ function procesarSolicitudPublicacion(request){
     } 
     // De caso contrario, es necesario dar de alta el nuevo topico
     else {
-        enviarBrokerSegunTopicoNoExistente(topico,isPub);
+        enviarAsignacionTopicoBroker(reqSocket, topico,isPub);
     }
 }
 
@@ -167,11 +182,4 @@ function procesarAltaCliente(request){
 
 
 
-// TO DO: Abstraer a global.js
-// Genera UUID a fin de ser utilizado como ID de mensaje.
-function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-}
+
---
+++ Global/Globals.js
@@ -0,0 +1,97 @@
+
+//---------------------------<Codigos de accion para REQREP>----------------------------------------------
+
+//CLIENTE -> COORDINADOR
+const COD_PUB = 1; // Cliente publica un nuevo mensaje
+
+const COD_ALTA_SUB = 2; // Cliente se suscribe a un tópico
+
+
+//COORDINADOR -> BROKER
+const COD_ADD_TOPICO_BROKER = 3;// Coordinador informa al broker un nuevo topico 
+
+
+//SERVIDOR HTTP -> BROKER
+const COD_GET_TOPICOS = 4; // Servidor solicita a todos los sus topicos 
+
+const COD_GET_MENSAJES_COLA = 5;// Servidor solicita a todos los brokers todos sus mensajes
+
+const COD_BORRAR_MENSAJES = 6; // Servidor solicita a un broker que borre sus mensajes
+
+//---------------------------</Codigos de accion para REQREP>----------------------------------------------
+
+
+//---------------------------<Codigos de error para REQREP>-----------------------------------------------
+
+const COD_ERROR_TOPICO_INEXISTENTE = 1
+const COD_ERROR_OPERACION_INEXISTENTE = 2
+
+
+
+
+//---------------------------</Codigos de error para REQREP>-----------------------------------------------
+
+
+
+
+
+// retorna una respuesta exitosa con los resultados especificados
+function generarRespuestaExitosa(accion,idPeticion,resultados) {
+
+	return new {
+		exito: true,
+		accion: accion,
+		idPeticion: idPeticion,
+		resultados: resultados
+	}
+}
+
+
+// retorna una respuesta no exitosa con el codigo de error y mensaje especificados
+function generarRespuestaNoExitosa(accion, idPeticion, codigoError, mensajeError) {
+
+	return new {
+		exito: false,
+		accion: accion,
+		idPeticion: idPeticion,
+		error: {
+			codigo: codigoError,
+			mensaje: mensajeError
+		}
+	}
+}
+
+
+// Genera UUID a fin de ser utilizado como ID de mensaje.
+function generateUUID() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
+
+
+// dado un arreglo asociativo keyvalue retorna la cantidad de keys (elementos) que contiene
+function getCantKeys(keyvalue) {
+	return Object.keys(keyvalue).length
+}
+
+
+module.exports = {
+	// constantes
+	COD_PUB: COD_PUB,
+    COD_ALTA_SUB: COD_ALTA_SUB,
+    COD_ADD_TOPICO_BROKER: COD_ADD_TOPICO_BROKER,
+    COD_GET_TOPICOS: COD_GET_TOPICOS,
+    COD_GET_MENSAJES_COLA: COD_GET_MENSAJES_COLA,
+	COD_BORRAR_MENSAJES: COD_BORRAR_MENSAJES,
+
+	COD_ERROR_TOPICO_INEXISTENTE: COD_ERROR_TOPICO_INEXISTENTE,
+	COD_ERROR_OPERACION_INEXISTENTE: COD_ERROR_OPERACION_INEXISTENTE,
+
+	// funciones
+	generarRespuestaExitosa: generarRespuestaExitosa,
+	generarRespuestaNoExitosa: generarRespuestaNoExitosa,
+	generateUUID: generateUUID,
+	getCantKeys: getCantKeys
+}
